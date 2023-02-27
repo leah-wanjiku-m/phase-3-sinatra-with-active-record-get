4abb9c8485d0dbe383f81e3b8bd895a14af98814
leah wanjiku <leagm22@gmail.com>
2023-02-27 10:26:16 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
-    { message: "Hello world" }.to_json
+    { message: "Hello world" }.to_json(:title).limit(10)
   end
+  get '/games' do
+    # get all the games from the database
+    games = Game.all.order
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+  end
+
 
 end
